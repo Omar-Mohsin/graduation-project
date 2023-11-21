fe142927fb8171d11051b419841e6a6a402d0271
Omar Mohsin <Omarabdessalam4748@gmail.com>
2023-11-21 20:47:53 +0300

    fixing issues

--- final-project/app/Orders/page.js
+++ final-project/app/Orders/page.js
@@ -1,41 +1,114 @@
-'use client'
+'use client';
 import React from "react";
 import styled from "styled-components";
 import { useSelector } from "react-redux";
 import { SelectUser } from "@/redux/auth/authSlice";
 import Link from "next/link";
-import { useState , useEffect } from "react";
-function page() {
+import { useState, useEffect } from "react";
+
+function Page() {
   const user = useSelector(SelectUser);
-  return(
-    
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`http://127.0.0.1:8000/orders/api/user-orders/${user.id}/`);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        const jsonData = await response.json();
+        setOrders(jsonData.orders || []);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, [user?.id]);
+
+  return (
     <PageContainer>
-    
-    { 
-
-    user ? (
-
-      <></>
-    ) : (
-
-      <>
-      <p>You are not athorize to this page <Link href={'/SignIn'} style={{color : 'blue', cursor : 'pointer'}}>  please sign in</Link></p>
-
-      </>
-    )
-
-  }
-  </PageContainer>
-  )
-  ;
+      {user ? (
+        <OrderContainer>
+          {orders.length > 0 ? (
+            orders.map((order) => (
+              <OrderCard key={order.order_id}>
+                <h1>Order ID: {order.order_id}</h1>
+                <p>Total Price: ${order.total_price.toFixed(2)}</p>
+                <ul>
+                  {order.items.map((item, index) => (
+                    <li key={index}>{item.name} - Quantity: {item.quantity}</li>
+                  ))}
+                </ul>
+              </OrderCard>
+            ))
+          ) : (
+            <p>Loading...</p>
+          )}
+        </OrderContainer>
+      ) : (
+        <p>
+          You are not authorized to access this page{" "}
+          <Link href={"/LogIn"} style={{ color: "blue", cursor: "pointer" }}>
+            please sign in
+          </Link>
+        </p>
+      )}
+    </PageContainer>
+  );
 }
 
-export default page;
 const PageContainer = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-  background-color: #f4f4f4; 
-  height :   100vh;  
+margin-top:100px;
+display:flex;
+justify-content: center; /* Center the cards in the container */
+
 `;
+
+const OrderContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center; /* Center the cards in the container */
+  gap: 20px;
+  max-width: 800px;
+`;
+
+const OrderCard = styled.div`
+  background-color: #ffffff;
+  border-radius: 12px;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
+  padding: 20px;
+  width: 100%;
+  max-width: 300px;
+  text-align: left;
+  transition: transform 0.2s;
+
+  &:hover {
+    transform: translateY(-5px);
+  }
+
+  h1 {
+    font-size: 20px;
+    margin-bottom: 10px;
+    color: #333;
+  }
+
+  p {
+    margin-bottom: 12px;
+    color: #666;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+
+  li {
+    margin-bottom: 6px;
+    color: #888;
+  }
+`;
+
+export default Page;
--- final-project/app/Statistics/page.js
+++ final-project/app/Statistics/page.js
@@ -2,47 +2,43 @@
 import React from "react";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
-import axios from "axios";
-import { SelectAllProducts } from "@/redux/products/productsSlice";
-import { useSelector } from "react-redux";
 
 function Page() {
   const [data, setData] = useState(null);
-  const products = useSelector(SelectAllProducts);
-  const firstFiveProducts = products.slice(0, 3);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(''); // Put your URL here
+        const response = await fetch("http://localhost:8000/api/statistics/"); // Put your URL here
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
         const jsonData = await response.json();
+        console.log(jsonData);
         setData(jsonData);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
     fetchData();
-  }, [data]); 
-
+  }, []);
+  console.log(data);
   return (
     <>
-     <TitlePage>Top  3 Items</TitlePage>
+      <TitlePage>Top 3 Items</TitlePage>
 
-    <Container>
-      {firstFiveProducts.map((product) => (
-        <ProductCard key={product.id}>
-          <ImageContainer>
-            <img src={product.image}></img>
-          </ImageContainer>
-          <ProductTitle>{product.title}</ProductTitle>
-          <ProductPrice>{product.price}</ProductPrice>
-          <QuantitySold>Quantity Sold: {product.quantity_sold}</QuantitySold>
-        </ProductCard>
-      ))}
+      <Container>
+        {data?.top_items.map((product) => (
+          <ProductCard>
+            <ImageContainer>
+              <img src={product.image_url}></img>
+            </ImageContainer>
+            <ProductTitle>{product.name}</ProductTitle>
+            <ProductPrice>${product.price}</ProductPrice>
+            <QuantitySold>Quantity Sold: {product.quantity_sold}</QuantitySold>
+          </ProductCard>
+        ))}
       </Container>
     </>
   );
@@ -50,86 +46,75 @@ function Page() {
 
 export default Page;
 
-
 const TitlePage = styled.h1`
-
-    text-align: center;
-    font-size: 2rem;
-    font-weight: 600;
-    margin-top: 1rem;
-    
-`
+  text-align: center;
+  font-size: 2rem;
+  font-weight: 600;
+  margin-top: 1rem;
+`;
 
 const Container = styled.div`
   margin: 0 auto;
   padding: 2rem;
   text-align: center;
   display: flex;
-
 `;
 
 const ProductCard = styled.div`
-background: #fff;
-padding: 1rem;
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-border-radius: 0.375rem;
-transition: transform 0.3s, box-shadow 0.3s;
+  background: #fff;
+  padding: 1rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  border-radius: 0.375rem;
+  transition: transform 0.3s, box-shadow 0.3s;
 
-&:hover {
-  transform: scale(1.05);
-  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-}
+  &:hover {
+    transform: scale(1.05);
+    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+  }
 
-transform-style: preserve-3d;
-backface-visibility: hidden;
-  
+  transform-style: preserve-3d;
+  backface-visibility: hidden;
 `;
 
 const ImageContainer = styled.div`
-width: 50%;
-height: 50%;
-padding-bottom: 150px;
-overflow: hidden;
-border-radius: 0.375rem;
+  width: 50%;
+  height: 50%;
+  padding-bottom: 150px;
+  overflow: hidden;
+  border-radius: 0.375rem;
 
-img {
-  object-fit: cover;
-  width: "100%";
-  height: "100%";
-  padding: 50px;
-  transition: opacity 0.3s;
-  &:hover {
-    opacity: 0.9;
+  img {
+    object-fit: cover;
+    width: "100%";
+    height: "100%";
+    padding: 50px;
+    transition: opacity 0.3s;
+    &:hover {
+      opacity: 0.9;
+    }
   }
-}
 `;
 
-
 const ProductTitle = styled.h2`
-font-size: 1.5rem;
-font-weight: 600;
-margin-top: 1rem;
-white-space: nowrap;
-overflow: hidden;
-text-overflow: ellipsis;
+  font-size: 1.5rem;
+  font-weight: 600;
+  margin-top: 1rem;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 const ProductDescription = styled.div``;
 
 const ProductPrice = styled.p`
-color: #666;
-font-size: 1.25rem;
-margin-top: 0.5rem;
+  color: #666;
+  font-size: 1.25rem;
+  margin-top: 0.5rem;
 `;
 const QuantitySold = styled.div`
-
-color : green;
+  color: green;
 `;
-
-
-
-
